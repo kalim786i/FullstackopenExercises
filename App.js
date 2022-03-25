1066472f2106e8fd1d483553f5f855960494ff30
kalim786i <44832633+kalim786i@users.noreply.github.com>
2022-03-25 10:48:33 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,66 +1,57 @@
 const Header= (props) =>{
-return(
-<h1>{props.course}</h1>
-)
-}
-
-const Content= (props) =>{
-  const part=props.part
-   
   return(
-    <p>
-      {part.map((value) =><>{value.name}:<br></br>{value.exercises}<br></br></>)}
-    </p>
+  <h1>{props.course}</h1>
   )
   }
-  const Part = (props)=>{
+  
+  const Content= (props) =>{
     const part=props.part
-  return(
+     
+    return(
       <p>
-    {part.name} {part.exercises}
-  </p>
+        {part.map((value) =><>{value.name}:<br></br>{value.exercises}<br></br></>)}
+      </p>
+    )
+    }
+      const Total = (props) =>{
+  const part=props.part
+  let total=0;
+  part.forEach(
+    value =>{ 
+      total=total+value.exercises
+    })
+  return(
+    <>
+    <p>Number of exercises: {total}</p>
+    </>
+  )
+  
+  }
+  const App = () => {
+    const course = 'Half Stack application development'
+    const parts = [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  
+  
+    return (
+      <div>
+        <Header course={course}/>
+        <Content part={parts} />
+        <Total part={parts}  />
+      </div>
     )
   }
-
-const Total = (props) =>{
-const part=props.part
-let total=0;
-part.forEach(
-  value =>{ 
-    total=total+value.exercises
-  })
-return(
-  <>
-  <p>Number of exercises: {total}</p>
-  </>
-)
-
-}
-const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-
-
-  return (
-    <div>
-      <Header course={course}/>
-      <Content part={parts} />
-      <Total part={parts}  />
-    </div>
-  )
-}
-
-export default App
+  
+  export default App
